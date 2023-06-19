8fe55b0fd4a30118b134c9607682eaf14a5afce9
Ali Jabar <ali93456@gmail.com>
2023-06-19 16:43:43 +0300

    bug fixed, UI improvment, landing page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import Board from './containers/Board';
+import StartingPage from './containers/StartingPage';
+
+type theGameInterface = {
+  landingPage: boolean,
+  vsComputer: boolean,
+  vsFriend: boolean
+}
 
 function App() {
+
+  const [gameInterface, setGameInterface] = useState<theGameInterface>({ landingPage: true, vsComputer:false, vsFriend: false });  
+
+  const playWithAIHandler = (): void => {
+      setGameInterface({ landingPage: false, vsComputer:true, vsFriend: false });
+  }
+  
+  const playWithAFriendHandler = (): void => {
+      setGameInterface({ landingPage: false, vsComputer:false, vsFriend: true });
+  }
+
+  const exitGameHandler = (): void => {
+    setGameInterface({ landingPage: true, vsComputer:false, vsFriend: false });
+  }
+
   return (
-      <Board />
+    <>
+      {gameInterface.landingPage && <StartingPage onPlayWithFriend={playWithAFriendHandler} onPlayWithAI={playWithAIHandler}/>}
+      {(gameInterface.vsComputer || gameInterface.vsFriend) && <Board onExitGame={exitGameHandler}/>}
+    </>
   ); 
 }
 
-export default App
+export default App;
--- src/containers/Board.tsx
+++ src/containers/Board.tsx
@@ -6,17 +6,17 @@ type Player = "X" | "Y" | "Tie" | null;
 
 function calculateWinner(squares: Player[]): Player {
     const lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
 
-    for(let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (
             squares[a] &&
@@ -29,20 +29,25 @@ function calculateWinner(squares: Player[]): Player {
     return null;
 }
 
-const Board = () => {
+const Board = ({ onExitGame }: { onExitGame(): void }) => {
 
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>(Math.round(Math.random() * 1) === 1 ? 'X' : 'O');
     const [winner, setWinner] = useState<Player>(null);
 
-    function reset () {
+    function reset() {
         setSquares(Array(9).fill(null));
         setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? 'X' : 'O');
         setWinner(null);
     }
 
-    function setSquareValue (index: number): void {
-        const newData = squares.map((val, i:number) => {
+    function returnToStartingPage(): void {
+        reset();
+        onExitGame();
+    }
+
+    function setSquareValue(index: number): void {
+        const newData = squares.map((val, i: number) => {
             if (i === index) {
                 return currentPlayer;
             }
@@ -66,34 +71,37 @@ const Board = () => {
     }, [squares])
 
 
-    let content = 
+    let content =
         <div className={styles.info}>
             <p className={styles.whichPlayerTurn}>{currentPlayer}, it's your turn</p>
         </div>
 
-    if (winner) 
-        content = 
-        <div className={`${styles.info} ${styles.winner}`}>
-            <p className={`${styles.whichPlayerTurn} ${styles.winner}`}>🎉 Congratulations {winner} 🎉</p>
-        </div>
+    if (winner)
+        content =
+            <div className={`${styles.info} ${styles.winner}`}>
+                <p className={`${styles.whichPlayerTurn} ${styles.winner}`}>🎉 Congratulations {winner} 🎉</p>
+            </div>
 
-    if (winner === "Tie") 
-        content = 
-        <div className={`${styles.info} ${styles.tie}`}>
-            <p className={styles.whichPlayerTurn}>👎🏼 Damn, it's a Tie 👎🏼</p>   
-        </div>
+    if (winner === "Tie")
+        content =
+            <div className={`${styles.info} ${styles.tie}`}>
+                <p className={styles.whichPlayerTurn}>👎🏼 Damn, it's a Tie 👎🏼</p>
+            </div>
 
-  return (
-    <main className={styles.container}>
-        {content}
-        <div className={styles.grid}>
-            {Array(9).fill(null).map(
-                (_, i) => <Square winner={winner} key={i} onClick={() => setSquareValue(i)} value={squares[i]}/>
-            )}
-        </div>
-        <button className={styles.restart} onClick={reset}>Restart</button>
-    </main>
-  );
+    return (
+        <main className={styles.container}>
+            {content}
+            <div className={styles.grid}>
+                {Array(9).fill(null).map(
+                    (_, i) => <Square winner={winner} key={i} onClick={() => setSquareValue(i)} value={squares[i]} />
+                )}
+            </div>
+            <div className={styles.btnContainer}>
+                <button className={`${styles.boardBtn} ${styles.exit}`} onClick={returnToStartingPage}> Exit </button>
+                <button className={`${styles.boardBtn} ${styles.restart}`} onClick={reset}> Restart </button>
+            </div>
+        </main>
+    );
 }
 
 export default Board;
